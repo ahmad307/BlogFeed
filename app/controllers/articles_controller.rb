99d749e42e28cac8406b8e30dfd7adc21f0662ba
Ahmad <ahmad.hussain.307@gmail.com>
2019-02-23 21:36:10 +0200

    Added completed article workflow

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,37 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    article = Article.new
+    article.title = params[:article][:title]
+    article.body = params[:article][:body]
+    article.save
+    redirect_to article_path(article)
+  end
+
+  def destroy
+    id = params[:id]
+    Article.all.destroy(id)
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.title = params[:article][:title]
+    @article.body = params[:article][:body]
+    @article.save
+
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+    redirect_to article_path
+  end
 end
